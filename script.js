93618aad8d539f41d2495a9f67395cdd0dba293d
Gaurav Dahiya <dahiyag21@gmail.com>
2017-06-15 18:43:08 +0530

    First Javascript File

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Created by gaurav on 15/6/17.*/
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
