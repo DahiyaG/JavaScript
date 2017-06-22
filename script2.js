88244db1bc8e56dfdc96c109e3c0fe773d626ab9
Gaurav Dahiya <dahiyag21@gmail.com>
2017-06-22 18:48:45 +0530

    functions , statements and expressions

---
+++ script2.js
@@ -0,0 +1,56 @@
+/**
+ * Created by gaurav on 22/6/17.
+ */
+
+
+//////////////////////
+//Lecture : Functions
+/*
+function calculateAge(yearOfBirth) {
+    var age = 2017 - yearOfBirth;
+    return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1948);
+console.log(ageJohn);
+
+function yearsUntilRetirement(name,year) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if (retirement>=0){
+        console.log(name + ' retires in ' + retirement + ' years.');
+    }
+    else {
+        console.log(name + ' has already retired.');
+    }
+
+}
+
+yearsUntilRetirement('John',1990);
+yearsUntilRetirement('Mike',1969);
+yearsUntilRetirement('Mary',1948);
+*/
+
+
+///////////////////////////////////////
+//Lecture : Statements and expressions
+/*
+function someFun(par) {
+    //code
+}
+
+var someFun = function (par) {
+    //code
+}
+
+//Expressions
+3 + 4;
+var x = 3;
+
+//Statements
+if (x===3){
+    //do something
+}
+*/
