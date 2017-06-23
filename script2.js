79d3aad1e461e6fb5a971f18059321ce5d117770
Gaurav Dahiya <dahiyag21@gmail.com>
2017-06-23 19:03:36 +0530

    Arrays and objects

--- script2.js
+++ script2.js
@@ -54,3 +54,64 @@ if (x===3){
     //do something
 }
 */
+
+
+///////////////////
+//Lecture : Arrays
+/*
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+if (john.indexOf('designer') === -1){
+    console.log('John is NOT a designer.');
+}
+*/
+
+
+////////////////////
+//Lecture : Objects
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false
+};
+
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john['job'] = 'programmer';
+
+console.log(john);
+
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1969;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+*/
+
+
+////////////////////////////////
+//Lecture : Objects and methods
